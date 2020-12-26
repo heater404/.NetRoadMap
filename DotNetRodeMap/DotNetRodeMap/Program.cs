63193b9f137a4cf51dae35828fcc35f48647d71b
zhu daokuan <15549519416@163.com>
2020-12-26 14:10:39 +0800

    新建一个项目

---
+++ DotNetRodeMap/DotNetRodeMap/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace DotNetRodeMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matches = Regex.Match("wfwf\u000713r321", @"\a");
+            foreach (var group in matches.Groups)
+            {
+                Console.WriteLine(group.ToString());
+            }
+        }
+    }
+}
